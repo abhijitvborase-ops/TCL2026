23686b87dcd49f471390ce839a7cad0272ad2371
abhijitvborase-ops <abhijitvborase@gmail.com>
2026-03-24 10:45:42 +0000

    firebase auction working final

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -12,16 +12,34 @@ export class FirebaseService {
   constructor() {
 
     const firebaseConfig = {
-  apiKey: "AIzaSyBqpYV796eXqtahKx5O8QHdd0dUOCbHVZo",
-  authDomain: "tcl-2026.firebaseapp.com",
-  projectId: "tcl-2026",
-  storageBucket: "tcl-2026.firebasestorage.app",
-  messagingSenderId: "342890234890",
-  appId: "1:342890234890:web:a63c44d802f3a2ad958bf3",
-  measurementId: "G-HMPJ8SVZJM"
-};
+      apiKey: "AIzaSyBqpYV796eXqtahKx5O8QHdd0dUOCbHVZo",
+      authDomain: "tcl-2026.firebaseapp.com",
+      projectId: "tcl-2026",
+      storageBucket: "tcl-2026.firebasestorage.app",
+      messagingSenderId: "342890234890",
+      appId: "1:342890234890:web:a63c44d802f3a2ad958bf3",
+      measurementId: "G-HMPJ8SVZJM"
+    };
 
     const app = initializeApp(firebaseConfig);
     this.db = getFirestore(app);
+  }
+
+  // 🔥 ADD THIS FUNCTION
+  async uploadImage(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'tcl_upload'); // 👈 तुझं preset
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dzlyg7wog/image/upload',
+      {
+        method: 'POST',
+        body: formData
+      }
+    );
+
+    const data = await res.json();
+    return data.secure_url; // 👈 image URL return
   }
 }
